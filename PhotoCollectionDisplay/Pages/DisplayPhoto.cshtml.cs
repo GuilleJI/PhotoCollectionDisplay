351a62c378a6c474c5a7a38cd78543df0e68248d
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-02-16 17:34:57 -0400

    More UI updates.

--- PhotoCollectionDisplay/Pages/DisplayPhoto.cshtml.cs
+++ PhotoCollectionDisplay/Pages/DisplayPhoto.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PhotoCollectionDisplay.Data;
+using PhotoCollectionDisplay.Models;
 
 namespace PhotoCollectionDisplay.Pages
 {
     public class DisplayPhotoModel : PageModel
     {
-        public void OnGet()
+        private readonly PhotoCollectionDisplay.Data.PhotoCollectionDisplayContext _context;
+
+        public Photo Photo { get; set; } = default!;
+
+        // Constructor
+        public DisplayPhotoModel(PhotoCollectionDisplayContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photo.FirstOrDefaultAsync(m => m.PhotoId == id);
+
+            if (photo == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Photo = photo;
+            }
+            return Page();
         }
     }
 }
